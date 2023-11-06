560b6f884c64a280e9dff87f262fd1095666d795
qprinceqq <Dubrovin.rv.04@gmail.com>
2023-11-07 01:11:06 +0500

    Small changes in 09 module, start 10

--- homeworks/09_module/main.js
+++ homeworks/09_module/main.js
@@ -3,8 +3,8 @@
         list.sort(() => Math.random() - 0.5);
         return list;
     }
-
     function createCard(cardName, index){
+        let container = document.getElementById("container")
         let card = document.createElement("button");
         card.textContent = "Перевернуть!";
         card.classList.add("btn-light");
@@ -23,13 +23,13 @@
                 let card2 = document.getElementById(tryCards[1].id);
                 if (disableCount === 8) {
                     let reButton = document.createElement("button");
-                    reButton.classList.add("btn-outline-primary","btn-block","active");
-                    reButton.setAttribute("style", "height:150px");
-                    reButton.innerHTML = "<strong>СЫГРАТЬ ЕЩЕ РАЗ</strong>";
+                    reButton.classList.add("btn-info","btn-block");
+                    reButton.setAttribute("style", "height:120px");
+                    reButton.innerHTML = "<h2>СЫГРАТЬ ЕЩЕ РАЗ</h2>";
                     reButton.addEventListener("click", function(){
                         location.reload();
                     })
-                    document.body.append(reButton)
+                    container.append(reButton)
                 }
                 if (!card1.getAttribute("disabled") && !card2.getAttribute("disabled")){
                     card1.setAttribute("disabled", true);
@@ -49,8 +49,9 @@
 
     function createCardsApp(){
         let cardList = shuffleList([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]);
+        let container = document.getElementById("container")
         for (i in cardList){
-            document.body.append(createCard(cardList[i], i));
+            container.append(createCard(cardList[i], i));
         }
     }
 
